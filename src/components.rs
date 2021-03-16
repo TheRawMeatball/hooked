520d7133d11a92e012b5841ae0093afeb7790384
TheRawMeatball <therawmeatball@gmail.com>
2021-03-16 17:03:18 +0300

    remove excess pub, add TODO

--- src/components.rs
+++ src/components.rs
@@ -2,7 +2,7 @@ use std::collections::HashMap;
 
 use crate::internal::{Component, ComponentId};
 
-pub struct Components {
+pub(crate) struct Components {
     counter: u64,
     inner: HashMap<ComponentId, Component>,
 }
@@ -15,27 +15,27 @@ impl Components {
         }
     }
 
-    pub(crate) fn get(&mut self, id: ComponentId) -> &mut Component {
+    pub fn get(&mut self, id: ComponentId) -> &mut Component {
         self.inner.get_mut(&id).unwrap()
     }
 
-    pub(crate) fn take(&mut self, id: ComponentId) -> Component {
+    pub fn take(&mut self, id: ComponentId) -> Component {
         self.inner.remove(&id).unwrap()
     }
 
-    pub(crate) fn with<F: FnOnce(&mut Component, &mut Self)>(&mut self, id: ComponentId, f: F) {
+    pub fn with<F: FnOnce(&mut Component, &mut Self)>(&mut self, id: ComponentId, f: F) {
         let mut val = self.take(id);
         f(&mut val, self);
         self.inner.insert(id, val);
     }
 
-    pub(crate) fn allocate(&mut self) -> ComponentId {
+    pub fn allocate(&mut self) -> ComponentId {
         let id = ComponentId(self.counter);
         self.counter += 1;
         id
     }
 
-    pub(crate) fn fill(&mut self, id: ComponentId, component: Component) {
+    pub fn fill(&mut self, id: ComponentId, component: Component) {
         self.inner.insert(id, component);
     }
 }
